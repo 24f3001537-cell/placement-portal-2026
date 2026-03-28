2e49b232557a2ff10a8e119edaf2dd4dddeff3b3
24f3001537-cell <24f3001537@ds.study.iitm.ac.in>
2026-03-28 16:41:27 +0530

    Milestone-PPA Auth_RBAC

--- models.py
+++ models.py
@@ -7,6 +7,8 @@ class Admin(db.Model):
     username = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(100))
 
+
+
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
